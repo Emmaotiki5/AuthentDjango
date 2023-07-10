1e4376a25aacfbd1b3a0848b1b6dcd0c5d8fddbd
Emmaotiki5 <otikie507@gmail.com>
2023-07-10 11:40:32 +0100

    Vercel Training

--- Details/urls.py
+++ Details/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -9,3 +11,4 @@ urlpatterns = [
     path ('logout', views.logout, name='logout'),
     path('register', views.register, name='register'),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
