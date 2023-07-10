61ebdf57a9fc8c6e0da62bb419b91c5306562e70
Emmaotiki5 <otikie507@gmail.com>
2023-07-10 11:50:17 +0100

    Vercel Training

--- authentication/settings.py
+++ authentication/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-*ji-6t&d09-frmtc=h($mt7q-k2%+g%9z16a#*s9k1wd1us+dq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh','localhost','127.0.0.1', 'animestream-omuh.onrender.com']
 
 
 # Application definition
