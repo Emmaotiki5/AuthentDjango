30181a380f07dd5e41e05a7974b7a501c532d48b
Emmaotiki5 <otikie507@gmail.com>
2023-07-11 04:14:57 +0100

    Vercel Training

--- Details/views.py
+++ Details/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
 from django.contrib import messages
 from .models import Anime
-from gogoanimeapi import gogoanime as anime
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
